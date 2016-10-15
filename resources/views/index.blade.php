64d3c0e73199fdc373e5ea6e22ebd5d16b79b4a5
asengardeon <asengardeons@hotmail.com>
2016-10-15 15:16:36 -0300

    Commit utilizando politicas de autorização

---
+++ resources/views/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Create Task Form... -->
+
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <tr>
+                                        <!-- Task Name -->
+                                        <td class="table-text">
+                                            <div>{{ $task->name }}</div>
+                                        </td>
+
+                                        <!-- Delete Button -->
+                                        <td>
+                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,49 +19,12 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-/**
- * Show Task Dashboard
- */
-Route::get('/', function () {
-      $tasks = Task::orderBy('created_at', 'asc')->get();
 
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-Route::delete('/task/{task}', function (Task $task) {
-    //
-});
-/**
- * Add New Task
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
- * Delete Task
- */
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
-});
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
