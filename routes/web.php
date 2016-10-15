23b43865cad6a302c7ce926cd10dbc70e2b348b8
asengardeon <asengardeons@hotmail.com>
2016-10-15 13:23:53 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+/**
+ * Show Task Dashboard
+ */
+Route::get('/', function () {
+      $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
